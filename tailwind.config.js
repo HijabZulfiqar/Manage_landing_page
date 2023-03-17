26f05f81559a5449726a8a18f25a13b0d17e72b6
Hijab Zulfiqar <hijabzulfiqar2001@gmail.com>
2023-03-17 16:08:03 +0500

    manage-landing-page

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*.{html,js}"],
+  theme: {
+    screens:{
+      sm: '480px',
+      md: '860px',
+      lg: '1010px',
+      xl: '1440px',
+    },
+    extend: {},
+  },
+  plugins: [],
+}
